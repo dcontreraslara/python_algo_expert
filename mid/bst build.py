b195ea41d219b6c1af52cca4bb7ab97f8b8fe3a8
Dario Alonso Contreras Lara <dario.contreras@globant.com>
2021-01-08 16:29:16 -0300

    bst

---
+++ mid/bst build.py
@@ -0,0 +1,91 @@
+# Do not edit the class below except for
+# the insert, contains, and remove methods.
+# Feel free to add new properties and methods
+# to the class.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+        return self
+
+    def contains(self, value):
+        if self.value == value:
+            return True
+
+        if self.left is not None and value < self.value:
+            return self.left.contains(value)
+
+        if self.right is not None and value > self.value:
+            return self.right.contains(value)
+
+        return False
+
+    def getMin(self):
+        if self.left is None:
+            return self.value
+        else:
+            return self.left.getMin()
+
+    def remove(self, value, parent=None):
+        if value < self.value:
+            if self.left is not None:
+                self.left.remove(value, self)
+        elif value > self.value:
+            if self.right is not None:
+                self.right.remove(value, self)
+        else:
+            if self.left is not None and self.right is not None:
+                self.value = self.right.getMin()
+                self.right.remove(self.value, self)
+            elif parent is None:
+                if self.left is not None:
+                    self.value = self.left.value
+                    self.right = self.left.right
+                    self.left = self.left.left
+                elif self.right is not None:
+                    self.value = self.right.value
+                    self.left = self.right.left
+                    self.right = self.right.right
+                else:
+                    print("single node avoid")
+                    pass
+            elif parent.left == self:
+                parent.left = self.left if self.left is not None else self.right
+            elif parent.right == self:
+                parent.right = self.left if self.left is not None else self.right
+        return self
+
+
+import unittest
+
+root = BST(10)
+root.left = BST(5)
+root.left.left = BST(2)
+root.left.left.left = BST(1)
+root.left.right = BST(5)
+root.right = BST(15)
+root.right.left = BST(13)
+root.right.left.right = BST(14)
+root.right.right = BST(22)
+
+root.insert(12)
+print(root.right.left.left.value == 12)
+
+root.remove(10)
+print(root.contains(10))
+print(root.value == 12)
+
+print(root.contains(15))
